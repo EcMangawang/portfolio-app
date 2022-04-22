ce694180397c4b28c69976c38630a4dbdd1c3027
EcMangawang <emmari.mangawang@edu.sait.ca>
2022-04-22 16:36:39 -0600

    Added picture and changed name

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,17 @@
+import profilePicture from './emmaripicture.jpg';
+console.log(profilePicture);
+
 function Header() {
     return (
         <header className="text-white bg-dark">
         <div className="container py-5">
             <div className="row">
                 <div className="col-xs-3 col-lg-2 text-center">
-                    <img src="https://www.seekpng.com/png/detail/110-1100707_person-avatar-placeholder.png"
-                        alt="Person - Avatar Placeholder@seekpng.com" className="border border-5 rounded-circle img-fluid" style={{width: 150, height: 150}}/>
+                    <img src={profilePicture}
+                        alt="Emmari Mangawang" className="border border-5 rounded-circle img-fluid" />
                 </div>
                 <div className="col text-center text-lg-start">
-                    <h1>Joe Blow</h1>
+                    <h1>Emmari Mangawang</h1>
 
                     <p className="lead">Software development student at SAIT (Southern Alberta Institute of Technology).</p>
 
