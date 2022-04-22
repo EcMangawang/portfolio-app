fca8ec5d1de515e8873acd10b486ed8336a8a030
EcMangawang <emmari.mangawang@edu.sait.ca>
2022-04-22 16:09:58 -0600

    Separated out Header component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-
 import './App.css';
+import Header from './Header.js';
 
 function App() {
   return (
     <div className="App">
-       <header className="text-white bg-dark">
-        <div className="container py-5">
-            <div className="row">
-                <div className="col-xs-3 col-lg-2 text-center">
-                    <img src="https://www.seekpng.com/png/detail/110-1100707_person-avatar-placeholder.png"
-                        alt="Person - Avatar Placeholder@seekpng.com" className="border border-5 rounded-circle img-fluid" style={{width: 150, height: 150}}/>
-                </div>
-                <div className="col text-center text-lg-start">
-                    <h1>Joe Blow</h1>
+        <Header />
 
-                    <p className="lead">Software development student at SAIT (Southern Alberta Institute of Technology).</p>
-
-                    <ul className="list-inline">
-                        <li className="list-inline-item my-2"><a href="#" className="fs-5 link-light text-decoration-none"><i
-                                    className="bi bi-envelope pe-1"></i>
-                                <span className="text-center">E-mail</span></a></li>
-                        <li className="list-inline-item my-2"><a href="#" className="fs-5 link-light text-decoration-none"><i
-                                    className="bi bi-linkedin pe-1"></i>
-                                LinkedIn</a></li>
-                        <li className="list-inline-item my-2"><a href="#" className="fs-5 link-light text-decoration-none"><i
-                                    className="bi bi-github pe-1"></i>
-                                GitHub</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </header>
     <main className="container">
         <div className="row mt-3">
             <div className="col-md">
